d5aaef88cc312b9f61fa86ef3ef77f0d131f7862
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2024-07-17 14:25:14 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,59 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title("Streamlit App :balloon:")
+
+name_on_order = st.text_input("name on smoothie")
+st.write("name on smoothie will be :",name_on_order)
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+INGREDIENT_LIST = st.multiselect('choose upto 5 ingredients:',my_dataframe,max_selections =5)
+if INGREDIENT_LIST :
+    # st.write(INGREDIENT_LIST)
+    # st.text(INGREDIENT_LIST)
+
+    ingredient_string =''
+    for fruit in INGREDIENT_LIST :
+        ingredient_string +=fruit  + ' '
+    # st.write(ingredient_string)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredient_string + """','""" + name_on_order + """')"""
+    
+
+    st.write(my_insert_stmt)
+    time_tp_insert = st.button("Submit order")
+    
+    if time_tp_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
+
+        
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
